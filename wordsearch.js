8fc10c62731307665734281a6b38bf041ca1a1c5
jpared3s <jparedes.wall@gmail.com>
2023-01-18 17:14:58 -0500

    word search assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    // Put your solution here
+    let newArr = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      newArr[i] = [];
+      for (let j = 0; j < matrix.length; j++) {
+        newArr[i].push(matrix[j][i]);
+      }
     }
-}
+    return newArr;
+  };
+  
+  
+  
+  
+  const wordSearch = (letters, word) => { 
+      let horizontalJoin = letters.map(ls => ls.join(''))
+      for (l of horizontalJoin) {
+          if (l.includes(word)) return true
+      }
+  
+      horizontalJoin = transpose(letters).map(ls => ls.join(''))
+      for (l of horizontalJoin) {
+          if (l.includes(word)) return true
+      }
+  
+      return false;
+  
+  }
 
 module.exports = wordSearch
